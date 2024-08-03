4b4af8983bfe1a4de718741ac70ab9338d2b77ab
Aleksandr Aleksandrov <mortyleano@MacBook-Pro-Aleksandr.local>
2024-08-03 18:36:33 +0300

    added fixes

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -30,14 +30,12 @@ public class RegistrationPage {
     @Step("Открываем страницу регистрации студента")
     public RegistrationPage openRegistrationPage() {
         open("/automation-practice-form");
-        stateInput.scrollTo();
         practiceFormWrapper.shouldHave(text("Student Registration Form"));
         return this;
     }
 
     @Step("Удаляем все рекламные баннера на странице регистрации студента")
     public RegistrationPage removeBanner() {
-        executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("document.querySelectorAll('#fixedban').forEach(el => el.remove());");
         executeJavaScript("$('footer').remove()");
         return this;
